11a05d551a4ef1a5d99bc827dbafc80b50171072
FTsune <s2021102102@firstasia.edu.ph>
2024-09-16 16:36:39 +0800

    Adjusted team.py

--- team.py
+++ team.py
@@ -33,22 +33,22 @@ def main():
         <div style="text-align: center;">
             <img src="./app/static/nico.png" style="margin: 5px; padding: 2px; border: 3px solid #41B3A2; border-radius: 100%; max-width: 100%; height: auto; max-height: 150px;">
             <p style='color: #41B3A2; text-align: center; font-weight: bold; max-width: 100%; height: auto;'>NICO</p>
-            <p style='color: white; margin-top: -20px; text-align: center; max-width: 100%; height: auto;'>Deep Learning Engineer</p>
+            <p style='color: black; margin-top: -20px; text-align: center; max-width: 100%; height: auto;'>Deep Learning Engineer</p>
         </div>
         <div style="text-align: center;">
             <img src="./app/static/kurt.jpg" style="margin: 5px; padding: 2px; border: 3px solid #41B3A2; border-radius: 100%; max-width: 100%; height: auto; max-height: 150px;">
             <p style='color: #41B3A2; text-align: center; font-weight: bold; max-width: 100%; height: auto;'>KURT</p>
-            <p style='color: white; margin-top: -20px; text-align: center; max-width: 100%; height: auto;'>Full Stack Developer</p>
+            <p style='color: black; margin-top: -20px; text-align: center; max-width: 100%; height: auto;'>Full Stack Developer</p>
         </div>
         <div style="text-align: center;">
             <img src="./app/static/franco.jpg" style="margin: 5px; padding: 2px; border: 3px solid #41B3A2; border-radius: 100%; max-width: 100%; height: auto; max-height: 150px;">
             <p style='color: #41B3A2; text-align: center; font-weight: bold; max-width: 100%; height: auto;'>FRANCO</p>
-            <p style='color: white; margin-top: -20px; text-align: center; max-width: 100%; height: auto;'>Python Developer</p>
+            <p style='color: black; margin-top: -20px; text-align: center; max-width: 100%; height: auto;'>Python Developer</p>
         </div>
         <div style="text-align: center;">
             <img src="./app/static/marvin.png" style="margin: 5px; padding: 2px; border: 3px solid #41B3A2; border-radius: 100%; max-width: 100%; height: auto; max-height: 150px;">
             <p style='color: #41B3A2; text-align: center; font-weight: bold; max-width: 100%; height: auto;'>MARVIN</p>
-            <p style='color: white; margin-top: -20px; text-align: center; max-width: 100%; height: auto;'>Data Cleaner</p>
+            <p style='color: black; margin-top: -20px; text-align: center; max-width: 100%; height: auto;'>Data Cleaner</p>
         </div>
     </div>
     <style>
