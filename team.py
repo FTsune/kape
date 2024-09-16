cead1434239d51b8dcdcbb7122acd352a44e76c3
FTsune <s2021102102@firstasia.edu.ph>
2024-09-16 18:21:21 +0800

    Adjusted UI

--- team.py
+++ team.py
@@ -109,28 +109,6 @@ def main():
                 <p class="title">MARVIN</p>
                 <p class="subtitle">Data Cleaner</p>
             </div>
-    <div class="grid-container">
-        <div style="text-align: center;">
-            <img src="./app/static/nico.png" style="margin: 5px; padding: 2px; border: 3px solid #41B3A2; border-radius: 100%; max-width: 100%; height: auto; max-height: 150px;">
-            <p style='color: #41B3A2; text-align: center; font-weight: bold; max-width: 100%; height: auto;'>NICO</p>
-            <p style='color: black; margin-top: -20px; text-align: center; max-width: 100%; height: auto;'>Deep Learning Engineer</p>
-        </div>
-        <div style="text-align: center;">
-            <img src="./app/static/kurt.jpg" style="margin: 5px; padding: 2px; border: 3px solid #41B3A2; border-radius: 100%; max-width: 100%; height: auto; max-height: 150px;">
-            <p style='color: #41B3A2; text-align: center; font-weight: bold; max-width: 100%; height: auto;'>KURT</p>
-            <p style='color: black; margin-top: -20px; text-align: center; max-width: 100%; height: auto;'>Full Stack Developer</p>
-        </div>
-        <div style="text-align: center;">
-            <img src="./app/static/franco.jpg" style="margin: 5px; padding: 2px; border: 3px solid #41B3A2; border-radius: 100%; max-width: 100%; height: auto; max-height: 150px;">
-            <p style='color: #41B3A2; text-align: center; font-weight: bold; max-width: 100%; height: auto;'>FRANCO</p>
-            <p style='color: black; margin-top: -20px; text-align: center; max-width: 100%; height: auto;'>Python Developer</p>
-        </div>
-        <div style="text-align: center;">
-            <img src="./app/static/marvin.png" style="margin: 5px; padding: 2px; border: 3px solid #41B3A2; border-radius: 100%; max-width: 100%; height: auto; max-height: 150px;">
-            <p style='color: #41B3A2; text-align: center; font-weight: bold; max-width: 100%; height: auto;'>MARVIN</p>
-            <p style='color: black; margin-top: -20px; text-align: center; max-width: 100%; height: auto;'>Data Cleaner</p>
-        </div>
-    </div>
     '''
 
     st.markdown(grid_html, unsafe_allow_html=True)
