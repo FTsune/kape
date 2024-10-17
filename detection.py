5f8097011c82776667acea3a4bc8c25daa2cfef1
FTsune <s2021102102@firstasia.edu.ph>
2024-10-17 21:01:31 +0800

    Minor change

--- detection.py
+++ detection.py
@@ -294,7 +294,7 @@ def main(theme_colors):
                 }}
                 """,
         ):
-            col1, col2 = st.columns()
+            col1, col2 = st.columns(2)
 
             with col1:
                 with st.container(border=True):
